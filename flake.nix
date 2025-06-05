e83f5694929ebfb043b80047d5c5f4ec3594e996
Anssi Lappalainen <anssilap@users.noreply.github.com>
2025-06-05 20:37:26 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
+
